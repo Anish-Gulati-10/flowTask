d75a5921447aa7c43ad11c4bbc869007ddc39553
Anish-Gulati-10 <theanishgulati@gmail.com>
2025-04-26 02:26:37 +0530

    feat: initialize client application with Vite and shadcn

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <section className='min-h-screen flex flex-col items-center justify-center'>
+      <Button>test</Button>
+    </section>
+      
+  )
+}
+
+export default App
